1d5689bbd730540bfacdec67a63f12cd2124472c
Dede Rahmat <dederamat102@gmail.com>
2024-06-11 09:28:40 +0700

    Add submission1-openmusic to .gitignore

---
+++ migrations/1718066670265_create-table-playlist-song-activities.js
@@ -0,0 +1,49 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("playlist_song_activities", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: "VARCHAR(50)",
+      notNull: true,
+      references: "playlists",
+      onDelete: "CASCADE",
+    },
+    song_id: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    user_id: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    action: {
+      type: "TEXT",
+      notNull: true,
+    },
+    time: {
+      type: "TEXT",
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable("playlist_song_activities");
+};
